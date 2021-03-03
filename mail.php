e1c1154ef8e6dc694818d888819c1616773c2f0a
aaburlaka <artbur@yandex.ru>
2021-03-04 01:55:17 +0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+
+	$recepient = 'youmail@ya.ru';
+	$sitename = 'Название сайта';
+
+	$name = trim($_GET['name']);
+	$phone = trim($_GET['phone']);
+	$text = trim($_GET['text']);
+
+	$subject = 'Новая заявка с сайта '.$sitename;
+	$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+
+	mail($recepient, $subject, $message, 'Content-type: text/plain; charset=\'utf-8\'\n From: $recepient');
+
+?>
